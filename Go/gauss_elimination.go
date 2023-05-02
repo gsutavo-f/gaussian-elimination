18db432b9bfa289cfa05ed93f76f9f234ded75fc
iyisakuma <yujisakuma224@gmail.com>
2023-05-01 22:01:29 -0300

    Adicionado o tempo decorrido no metodo ao resolver 100 mil vezes a matriz

--- Go/gauss_elimination.go
+++ Go/gauss_elimination.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 func gaussSolver(n int, A [][]float64, b []float64) []float64 {
@@ -53,40 +54,45 @@ func gaussSolver(n int, A [][]float64, b []float64) []float64 {
 	}
 	return x
 }
+func gauss_solver_executionTime(n int, A [][]float64, b []float64) (tempoDecorrido int64) {
+	inicio := time.Now().UnixNano() / int64(time.Millisecond)
 
+	for i := 0; i < 100000; i++ {
+		gaussSolver(n, A, b)
+	}
+	fim := time.Now().UnixNano() / int64(time.Millisecond)
+	tempoDecorrido = fim - inicio
+	fmt.Printf("Time elapsed in gauss_solver() is %v us\n\n", tempoDecorrido)
+	return tempoDecorrido
+}
 func main() {
+	var tempo_decorrido int64 = 0
 	n := 3
 	A := [][]float64{{2, 1, -1}, {1, 2, 1}, {1, 1, 1}}
 	b := []float64{-3, 3, 2}
-	var x []float64
-	x = gaussSolver(n, A, b)
-	fmt.Printf("x1 = %v\nx2 = %v\nx3 = %v\n", x[0], x[1], x[2])
+	tempo_decorrido = tempo_decorrido + gauss_solver_executionTime(n, A, b)
 
 	var A2 = [][]float64{{1, 1, 1}, {-2, 1, 1}, {1, 3, 1}}
 	var b2 = []float64{2, 5, 4}
-	x = gaussSolver(n, A2, b2)
-	fmt.Printf("\nx1 = %v\nx2 = %v\nx3 = %v\n", x[0], x[1], x[2])
+	tempo_decorrido = tempo_decorrido + gauss_solver_executionTime(n, A2, b2)
 
 	A3 := [][]float64{{3, 2, -1}, {2, -2, 4}, {-1, 0.5, -1}}
 	b3 := []float64{1, -2, 0}
-	x = gaussSolver(n, A3, b3)
-	fmt.Printf("\nx1 = %v\nx2 = %v\nx3 = %v\n", x[0], x[1], x[2])
+	tempo_decorrido = tempo_decorrido + gauss_solver_executionTime(n, A3, b3)
 
 	n = 2
 	A4 := [][]float64{{2, 3}, {4, 9}}
 	b4 := []float64{6, 15}
-	x = gaussSolver(n, A4, b4)
-	fmt.Printf("\nx1 = %v\nx2 = %v\n", x[0], x[1])
+	tempo_decorrido = tempo_decorrido + gauss_solver_executionTime(n, A4, b4)
 
 	n = 4
 	A5 := [][]float64{{4, 1, 2, -3}, {-3, 3, -1, 4}, {-1, 2, 5, 1}, {5, 4, 3, -1}}
 	b5 := []float64{-16, 20, -4, -10}
-	x = gaussSolver(n, A5, b5)
-	fmt.Printf("\nx1 = %v\nx2 = %v\nx3 = %v\nx4 = %v\n", x[0], x[1], x[2], x[3])
-
+	tempo_decorrido = tempo_decorrido + gauss_solver_executionTime(n, A5, b5)
 	n = 5
 	A6 := [][]float64{{4, 1, 2, -3, 5}, {-3, 3, -1, 4, -2}, {-1, 2, 5, 1, 3}, {5, 4, 3, -1, 2}, {1, -2, 3, -4, 5}}
 	b6 := []float64{-16, 20, -4, -10, 3}
-	x = gaussSolver(n, A6, b6)
-	fmt.Printf("\nx1 = %v\nx2 = %v\nx3 = %v\nx4 = %v\nx5 = %v\n", x[0], x[1], x[2], x[3], x[4])
+	tempo_decorrido = tempo_decorrido + gauss_solver_executionTime(n, A6, b6)
+
+	fmt.Printf("Total time elapsed in program is %v us", tempo_decorrido)
 }
